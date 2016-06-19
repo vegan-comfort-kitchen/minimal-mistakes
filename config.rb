0c842cdf36cbc2048767726c65e73aaf3e6fae1d
Joe Webb <imjoewebb@gmail.com>
2016-06-19 20:10:19 +0200

    Use haml

--- config.rb
+++ config.rb
@@ -18,6 +18,8 @@ page '/*.txt', layout: false
 
 # General configuration
 
+set :haml, { :ugly => true, :format => :html5 }
+
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
