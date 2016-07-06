ba16ab415573b4156a83b254bc8374cc9531b75a
Joe Webb <imjoewebb@gmail.com>
2016-07-06 21:52:44 +0200

    Second release, fixed header, changed font, text spacing improvements

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,8 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 var addMenuToggle = function(){
-  var menuToggle = document.querySelectorAll('.menu-collapsed')[0]
+  var menuToggle = document.querySelectorAll('.menu-toggle')[0]
+  var menu = document.querySelectorAll('.menu')[0]
   menuToggle.addEventListener('click', function(){
-    menuToggle.classList.toggle('menu-expanded');
+    menu.classList.toggle('menu-expanded');
+  });
+  menu.addEventListener('click', function(){
+    menu.classList.toggle('menu-expanded');
   });
 }
