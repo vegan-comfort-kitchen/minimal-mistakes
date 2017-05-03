3fc9f625fcd702747513483221490a597c8076d5
Joe Webb <joe.webb@solarisbank.de>
2017-05-02 21:43:29 -0400

    sdf

--- Makefile
+++
@@ -1,7 +0,0 @@
-haml-lint:
-	bundle exec haml-lint source
-
-rubocop:
-	bundle exec rubocop
-
-lint: rubocop haml-lint
---
+++ javascripts/all-a007a550.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){addMenuToggle()});var addMenuToggle=function(){var e=document.querySelectorAll(".nav-toggle")[0],n=document.querySelectorAll(".nav")[0];e.addEventListener("click",function(){n.classList.toggle("nav-expanded")}),n.addEventListener("click",function(){n.classList.toggle("nav-expanded")})};
