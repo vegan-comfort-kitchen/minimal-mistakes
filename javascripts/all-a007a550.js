5b6ed54047980e86194743b2f23637ad13ebbd79
Joe Webb <joe.webb@solarisbank.de>
2017-05-19 22:11:39 -0400

    Fix index layout issues, delete unneeded pages

--- javascripts/all-a007a550.js
+++ javascripts/all-a007a550.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded",function(){addMenuToggle()});var addMenuToggle=function(){var e=document.querySelectorAll(".nav-toggle")[0],n=document.querySelectorAll(".nav")[0];e.addEventListener("click",function(){n.classList.toggle("nav-expanded")}),n.addEventListener("click",function(){n.classList.toggle("nav-expanded")})};
+document.addEventListener("DOMContentLoaded", function() {
+  addMenuToggle()
+});
+var addMenuToggle = function() {
+  var e = document.querySelectorAll(".nav-toggle")[0],
+    n = document.querySelectorAll(".nav")[0];
+  e.addEventListener("click", function() {
+    n.classList.toggle("nav-expanded")
+  }), n.addEventListener("click", function() {
+    n.classList.toggle("nav-expanded")
+  })
+};
